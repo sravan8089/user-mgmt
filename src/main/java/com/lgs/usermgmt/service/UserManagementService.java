61a54864d97b53ccf4656a74555df6ce59140d26
sravan <sravan.sky9@gmail.com>
2023-10-09 11:26:23 -0400

    created a new file

--- src/main/java/com/lgs/usermgmt/service/UserManagementService.java
+++
@@ -1,9 +0,0 @@
-package com.lgs.usermgmt.service;
-
-import com.lgs.usermgmt.model.UserRegistrationRequest;
-import com.lgs.usermgmt.model.UserRegistrationResponse;
-
-public interface UserManagementService {
-
-    public UserRegistrationResponse registerUser(UserRegistrationRequest userRegistrationRequest);
-}
